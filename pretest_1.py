4a7300b201069d4c8e09b5b4fbc24636d3689c92
Xavier Nunez <xaviernunez83@gmail.com>
2021-04-15 11:58:05 -0700

    finish pseudocode

--- pretest_1.py
+++ pretest_1.py
@@ -15,8 +15,30 @@ function2 will loop 10 times, with each time, doubling the amount and add the am
 if the amount is equal, we output to the user" Option 1 and Option 2 are the same"
 if the option1 is better, we output to the user "option 1 is better"
 if the option2 is better, we output to the user "option 1 is better"
-
-
-
-
 """
+"""
+
+#  Option1 
+    return 100 * 10 
+
+#  Option2 
+    amount = 1
+    list1 = []
+    loop 10 times 
+        add amount to list1 
+        amount *= 2
+    sum = sum of all items in the loop 
+    return sum
+#  main
+    var1 = option1
+    var2 = option1
+
+    if var1 = var2
+        "option 1 and option2 are the same"
+     if var1 < var2
+        "option 2 is better"
+    if var2 < var1
+        "option 1 is better"
+
+    main 
+    """
