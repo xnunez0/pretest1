de6890098fbc7ee155730d2555c4245fa2d449f3
Xavier Nunez <xaviernunez83@gmail.com>
2021-04-15 12:31:45 -0700

    finish exam

--- pretest_1.py
+++ pretest_1.py
@@ -42,3 +42,32 @@ if the option2 is better, we output to the user "option 1 is better"
 
     main 
     """
+
+def option1():
+    return 100 * 10
+
+def option2(): 
+    amount = 1 
+    list1 = []
+    for i in range (0, 10):
+        list1.append(amount)
+        amount *= 2
+    print("list1", list1)
+    total = sum(list1) 
+    return total
+
+def main():
+    answer = ""
+    var1 = option1()
+    var2 = option2()
+    print("from main: var1", var1, 'var2', var2)
+    if var1 == var2:
+        answer = "option 1 and option2 are the same"
+    if var1 < var2:
+        answer = "option 2 is better"
+    if var2 < var1:
+        answer = "option 1 is better"
+    print(answer)
+
+
+main()
