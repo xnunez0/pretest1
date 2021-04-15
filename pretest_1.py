d21f7f58aad3fb43ea4857f8625219f58c31cdab
Xavier Nunez <xaviernunez83@gmail.com>
2021-04-15 08:15:01 -0700

    finish structured english

---
+++ pretest_1.py
@@ -0,0 +1,22 @@
+
+
+
+
+""""
+The program is trying to determine the best option(payment).
+The first option is ....
+the second option is...
+find a solutiion to see which option is best or if they are the same. 
+
+function1 will output 100 * 10 
+
+function2 will loop 10 times, with each time, doubling the amount and add the amount to the total.
+
+if the amount is equal, we output to the user" Option 1 and Option 2 are the same"
+if the option1 is better, we output to the user "option 1 is better"
+if the option2 is better, we output to the user "option 1 is better"
+
+
+
+
+"""
